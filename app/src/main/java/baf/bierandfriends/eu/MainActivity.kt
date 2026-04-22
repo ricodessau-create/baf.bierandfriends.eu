fc825e7d68c59c4c95b5cc7a07a7099d32100429
ricodessau-create <ricodessau@gmail.com>
2026-04-22 17:07:04 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/baf/bierandfriends/eu/MainActivity.kt
@@ -0,0 +1,22 @@
+package baf.bierandfriends.eu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+    }
+}
