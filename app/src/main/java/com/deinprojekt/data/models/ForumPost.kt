c3e208c5c6895e543927d3eb34b62510b8325430
ricodessau-create <ricodessau@gmail.com>
2026-04-23 06:01:27 +0200

    Create ForumPost.kt

---
+++ app/src/main/java/com/deinprojekt/data/models/ForumPost.kt
@@ -0,0 +1,8 @@
+package com.deinprojekt.data.models
+
+data class ForumPost(
+    val title: String = "",
+    val content: String = "",
+    val author: String = "",
+    val createdAt: Long = 0
+)
