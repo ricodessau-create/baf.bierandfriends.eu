2dc8a90a3cbb32cc53e10a988559e312e0144497
ricodessau-create <ricodessau@gmail.com>
2026-04-23 06:42:23 +0200

    Create MarketItem.kt

---
+++ app/src/main/java/com/deinprojekt/data/models/MarketItem.kt
@@ -0,0 +1,11 @@
+package com.deinprojekt.data.models
+
+import com.google.firebase.Timestamp
+
+data class MarketItem(
+    val createdAt: Timestamp? = null,
+    val description: String = "",
+    val ownerUuid: String = "",
+    val price: Double = 0.0,
+    val title: String = ""
+)
