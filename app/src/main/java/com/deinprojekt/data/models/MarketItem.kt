479daa99965dc1e8de3dafd1360bf782611485af
ricodessau-create <ricodessau@gmail.com>
2026-04-23 12:41:45 +0200

    Update MarketItem.kt

--- app/src/main/java/com/deinprojekt/data/models/MarketItem.kt
+++ app/src/main/java/com/deinprojekt/data/models/MarketItem.kt
@@ -3,6 +3,7 @@ package com.deinprojekt.data.models
 import com.google.firebase.Timestamp
 
 data class MarketItem(
+    val id: String = "",
     val createdAt: Timestamp? = null,
     val description: String = "",
     val ownerUuid: String = "",
