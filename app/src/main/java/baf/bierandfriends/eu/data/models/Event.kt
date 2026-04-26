7a66f351c3b43f5a241bba1be224366600815680
ricodessau-create <ricodessau@gmail.com>
2026-04-26 19:00:00 +0200

    Update Event.kt

--- app/src/main/java/baf/bierandfriends/eu/data/models/Event.kt
+++ app/src/main/java/baf/bierandfriends/eu/data/models/Event.kt
@@ -1,8 +1,10 @@
 package baf.bierandfriends.eu.data.models
 
+import com.google.firebase.Timestamp
+
 data class Event(
     val id: String = "",
     val name: String = "",
     val description: String = "",
-    val date: Long = 0
+    val date: Timestamp? = null
 )
