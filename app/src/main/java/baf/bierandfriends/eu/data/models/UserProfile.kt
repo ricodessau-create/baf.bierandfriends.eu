3d9642729370a803ff787a8314c8ccf74035f3fd
ricodessau-create <ricodessau@gmail.com>
2026-04-26 18:19:38 +0200

    Create UserProfile.kt

---
+++ app/src/main/java/baf/bierandfriends/eu/data/models/UserProfile.kt
@@ -0,0 +1,10 @@
+package baf.bierandfriends.eu.data.models
+
+data class UserProfile(
+    val username: String = "",
+    val email: String = "",
+    val rank: String = "",
+    val minecraftUuid: String = "",
+    val minecraftName: String = "",
+    val hopfenkaltschalen: Double = 0.0
+)
