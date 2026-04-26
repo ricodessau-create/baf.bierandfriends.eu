eac714bc2d74026ee9fa6e5f36975d39a31ddbc5
ricodessau-create <ricodessau@gmail.com>
2026-04-26 19:00:30 +0200

    Update ForumPost.kt

--- app/src/main/java/baf/bierandfriends/eu/data/models/ForumPost.kt
+++ app/src/main/java/baf/bierandfriends/eu/data/models/ForumPost.kt
@@ -1,10 +1,12 @@
 package baf.bierandfriends.eu.data.models
 
+import com.google.firebase.Timestamp
+
 data class ForumPost(
     val id: String = "",
     val title: String = "",
     val content: String = "",
     val author: String = "",
     val authorUid: String = "",
-    val createdAt: Long = 0
+    val createdAt: Timestamp? = null
 )
