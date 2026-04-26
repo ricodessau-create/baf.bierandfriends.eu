c239f2134f413f6661708ec1d4cf32712c1077ea
ricodessau-create <ricodessau@gmail.com>
2026-04-26 22:50:21 +0200

    Update CommunityFragment.kt

--- app/src/main/java/baf/bierandfriends/eu/ui/community/CommunityFragment.kt
+++ app/src/main/java/baf/bierandfriends/eu/ui/community/CommunityFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import baf.bierandfriends.eu.R
 import baf.bierandfriends.eu.data.repository.ForumRepository
 import baf.bierandfriends.eu.databinding.FragmentCommunityBinding
 import kotlinx.coroutines.launch
