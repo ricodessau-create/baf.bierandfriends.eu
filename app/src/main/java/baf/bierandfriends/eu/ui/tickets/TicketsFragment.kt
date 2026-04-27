b5bebef5599b5e6614198a452ea0b3e976c1fcc8
ricodessau-create <ricodessau@gmail.com>
2026-04-27 20:57:54 +0200

    Update TicketsFragment.kt

--- app/src/main/java/baf/bierandfriends/eu/ui/tickets/TicketsFragment.kt
+++ app/src/main/java/baf/bierandfriends/eu/ui/tickets/TicketsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import baf.bierandfriends.eu.R
 import baf.bierandfriends.eu.data.repository.TicketRepository
 import baf.bierandfriends.eu.databinding.FragmentTicketsBinding
@@ -30,6 +31,8 @@ class TicketsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupTabs()
         loadTickets()
 
         binding.newTicketButton.setOnClickListener {
@@ -37,15 +40,26 @@ class TicketsFragment : Fragment() {
         }
     }
 
+    private fun setupTabs() {
+        binding.tabMeineTickets.setOnClickListener {
+            binding.tabMeineTickets.setTextColor(resources.getColor(R.color.baf_gold, null))
+            binding.tabNeuesTicket.setTextColor(resources.getColor(R.color.baf_tab_unselected, null))
+            loadTickets()
+        }
+
+        binding.tabNeuesTicket.setOnClickListener {
+            findNavController().navigate(R.id.action_ticketsFragment_to_newTicketFragment)
+        }
+    }
+
     private fun loadTickets() {
         lifecycleScope.launch {
             val tickets = ticketRepository.getMyTickets()
-
             if (tickets.isNotEmpty()) {
+                binding.emptyText.visibility = View.GONE
                 val adapter = TicketsAdapter(tickets)
                 binding.ticketsRecyclerView.adapter = adapter
-                binding.ticketsRecyclerView.layoutManager =
-                    androidx.recyclerview.widget.LinearLayoutManager(requireContext())
+                binding.ticketsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
             } else {
                 binding.emptyText.visibility = View.VISIBLE
             }
