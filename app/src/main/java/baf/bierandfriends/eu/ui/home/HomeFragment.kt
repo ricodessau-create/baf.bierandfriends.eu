bc2c87c6eb7d4af0ba24f6593c296819c6fb81a6
ricodessau-create <ricodessau@gmail.com>
2026-04-27 20:57:19 +0200

    Update HomeFragment.kt

--- app/src/main/java/baf/bierandfriends/eu/ui/home/HomeFragment.kt
+++ app/src/main/java/baf/bierandfriends/eu/ui/home/HomeFragment.kt
@@ -6,7 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import baf.bierandfriends.eu.R
+import baf.bierandfriends.eu.data.repository.EventsRepository
+import baf.bierandfriends.eu.data.repository.ForumRepository
+import baf.bierandfriends.eu.data.repository.MarketRepository
 import baf.bierandfriends.eu.data.repository.NewsRepository
+import baf.bierandfriends.eu.data.repository.TicketRepository
 import baf.bierandfriends.eu.databinding.FragmentHomeBinding
 import kotlinx.coroutines.launch
 
@@ -16,6 +23,10 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val newsRepository = NewsRepository()
+    private val eventsRepository = EventsRepository()
+    private val forumRepository = ForumRepository()
+    private val ticketRepository = TicketRepository()
+    private val marketRepository = MarketRepository()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,20 +39,46 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.profileIcon.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+        }
+
         loadNews()
+        loadStats()
     }
 
     private fun loadNews() {
         lifecycleScope.launch {
             val newsList = newsRepository.getLatestNews()
             if (newsList.isNotEmpty()) {
+                binding.emptyText.visibility = View.GONE
                 val adapter = NewsAdapter(newsList)
                 binding.newsRecyclerView.adapter = adapter
-                binding.newsRecyclerView.layoutManager =
-                    androidx.recyclerview.widget.LinearLayoutManager(requireContext())
+                binding.newsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
             } else {
                 binding.emptyText.visibility = View.VISIBLE
             }
+        }
+    }
+
+    private fun loadStats() {
+        lifecycleScope.launch {
+            val events = eventsRepository.getUpcomingEvents()
+            binding.statEventsCount.text = events.size.toString()
+        }
+        lifecycleScope.launch {
+            val posts = forumRepository.getLatestPosts()
+            binding.statPostsCount.text = posts.size.toString()
+        }
+        lifecycleScope.launch {
+            val tickets = ticketRepository.getMyTickets()
+            val open = tickets.count { it.status == "offen" }
+            binding.statTicketsCount.text = open.toString()
+        }
+        lifecycleScope.launch {
+            val items = marketRepository.getMarketItems()
+            binding.statMarketCount.text = items.size.toString()
         }
     }
 
