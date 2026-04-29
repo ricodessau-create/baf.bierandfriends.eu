c2de52d5272a8bd6544c8d1516decd6637ec5016
ricodessau-create <ricodessau@gmail.com>
2026-04-29 05:36:35 +0200

    Update MarketItem.kt

--- app/src/main/java/baf/bierandfriends/eu/data/models/MarketItem.kt
+++ app/src/main/java/baf/bierandfriends/eu/data/models/MarketItem.kt
@@ -7,7 +7,10 @@ data class MarketItem(
     val createdAt: Timestamp? = null,
     val description: String = "",
     val ownerUuid: String = "",
+    val ownerName: String = "",
     val price: Double = 0.0,
     val title: String = "",
-    val imageUrl: String? = null
+    val imageUrl: String? = null,
+    val category: String = "sonstiges",
+    val type: String = "verkauf"
 )
