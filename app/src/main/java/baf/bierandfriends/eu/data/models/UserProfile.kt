0da0d4b48529952ce8682b6999ffb19087d99b50
ricodessau-create <ricodessau@gmail.com>
2026-05-02 14:20:48 +0200

    Add syncToken to UserProfile data class
    Added syncToken property to UserProfile data class.

--- app/src/main/java/baf/bierandfriends/eu/data/models/UserProfile.kt
+++ app/src/main/java/baf/bierandfriends/eu/data/models/UserProfile.kt
@@ -1,27 +1,19 @@
 package baf.bierandfriends.eu.data.models
 
 data class UserProfile(
-    // Identität & Account
     val username: String = "",
     val email: String = "",
     val rank: String = "",
     val photoUrl: String = "",
-    
-    // Minecraft Integration
     val minecraftUuid: String = "",
     val minecraftName: String = "",
-    
-    // Statistiken & Währung
     val hopfenkaltschalen: Double = 0.0,
-    
-    // Profil-Informationen & Social
-    val birthday: String = "", // Hier ist der Geburtstag enthalten
+    val birthday: String = "",
     val bio: String = "",
     val location: String = "",
     val discord: String = "",
-    
-    // Privatsphäre & Rechtliches (wichtig für die erste Version)
     val isPrivate: Boolean = false,
     val privacyAccepted: Boolean = false,
-    val termsAccepted: Boolean = false
+    val termsAccepted: Boolean = false,
+    val syncToken: String? = null
 )
